470505eb3b30e1bd37eee3627ef69ab0e94b22a0
xavier-meyer <xavier.01@orange.fr>
2024-01-05 15:01:40 +0100

    affichage des données dynamiques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'snapface 2.0';
+ 
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  // on déclare les propriétés 
+  title! : string;
+  description! : string;
+  createdDate! : Date;
+  snaps! : number;
+  imageUrl! : string;
+  flop! : number;
 
+  // la méthode ngOnInit est appellée au moment de la création d'une instance du component
+  // au moment de l'initialisation du component, on initialise les propriétés
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis longtemps';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.flop = 4;
+  }
+ 
 }
