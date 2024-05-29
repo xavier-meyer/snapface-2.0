afd2832d09cfd44ed0250d56a7a62ffcd08dab1e
xavier-meyer <xavier.01@orange.fr>
2024-05-29 11:32:20 +0200

    manipuler émissions d'observables avec opérateurs bas niveau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { map, filter, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+  interval$! : Observable<string>;
  
-  ngOnInit() {}   
+  ngOnInit() {
+    /* methode pipe: appliquer un opérateur à un observable ici, map est l'opérateur */
+    /* opérateur map : transformer les émissions des observables */
+    /* opérateur filter : filtrer les émissions */
+    this.interval$ = interval(1000).pipe(
+      filter( value => value % 3 === 0 && value > 0),
+      map( value => value % 2 === 0 ? 
+        `${ value } est un nombre pair et un nombre divisible par 3` :
+        `${ value } est un nombre impair et un nombre divisible par 3`
+      ),
+    tap( message => this.logger(message))
+    );
+  }   
+
+  logger(message: string) {
+    console.log(`${ message } et ceci s'autodétruira dans 20 secondes.`);
+  }
 }  
 
 
 
-
+  
